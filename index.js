3178251cce4e952fc7e157742a792d21dd7d59b9
gehrig <gehrigbarnes@gmail.com>
2022-10-13 23:21:04 -0500

    set up some simple questions in index.js. first funciton we want to add 1 to every element using forEach. and for subtractOne, we want to subtract 1 from every element. time to create the actual test

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+const numbers = [1,2,3,4,5]
+
+// numbers.forEach((num, index, array) => {
+//     num += 1
+//     array[index] = num;
+// })
+
+// console.log(numbers)
+
+const addOne = () => {
+  let newArray = []
+    //your code here.
+    
+}
+
+const subtractOne = () => {
+    let newArray = []
+    //your code here.
+    
+}
+
+
+
+
+
+
 
